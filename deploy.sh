c6942349df0de64671261eff5a491daae015bbc4
wingillis <win.gillis@gmail.com>
2016-11-13 20:15:28 -0500

    don't delete node modules

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ git checkout -b deploy
 
 bundle exec jekyll build
 
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name 'node_modules' ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 
 mv _site/* .
 rm -R _site/
