573d22636090e56857668cd1d321ad4f4f86c1e1
wingillis <win.gillis@gmail.com>
2016-11-13 23:13:48 -0500

    Added more images

--- _assets/scripts/gallery-main.js
+++ _assets/scripts/gallery-main.js
@@ -8,6 +8,6 @@ var app = new Vue({
   data: {
     x: null,
     y: null,
-    sources: ['https://i.imgur.com/x0liQTZm.png','https://i.imgur.com/F6ouLAtm.png','https://i.imgur.com/sitR5mPm.png','https://i.imgur.com/is39c1Km.png','https://i.imgur.com/wau8eyim.png','https://i.imgur.com/lsQTvU3m.png','https://i.imgur.com/kZXSf0lm.png','https://i.imgur.com/a4tTvQ4m.png','https://i.imgur.com/sOuw1Rkm.png','https://i.imgur.com/ynSsSJam.png', 'https://i.imgur.com/OJNQeR4m.png', 'https://i.imgur.com/OYYwVtrm.png']
+    sources: ['https://i.imgur.com/x0liQTZm.png','https://i.imgur.com/F6ouLAtm.png','https://i.imgur.com/sitR5mPm.png','https://i.imgur.com/is39c1Km.png','https://i.imgur.com/wau8eyim.png','https://i.imgur.com/lsQTvU3m.png','https://i.imgur.com/kZXSf0lm.png','https://i.imgur.com/a4tTvQ4m.png','https://i.imgur.com/sOuw1Rkm.png','https://i.imgur.com/ynSsSJam.png', 'https://i.imgur.com/OJNQeR4m.png', 'https://i.imgur.com/OYYwVtrm.png', 'https://i.imgur.com/WgdHWElm.png', 'https://i.imgur.com/UM2nv5mm.png', 'https://i.imgur.com/ROz3Ho2m.png', 'https://i.imgur.com/GfZaajAm.png', 'https://i.imgur.com/lSu34jhm.png', 'https://i.imgur.com/agumzygm.png']
   }
 });
