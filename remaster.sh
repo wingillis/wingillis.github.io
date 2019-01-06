ee4905b117fb25025fbae59cae7f6696210ea8cf
wingillis <win.gillis@gmail.com>
2019-01-06 16:44:35 -0500

    add line to merge upstream master

--- remaster.sh
+++ remaster.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-# git branch -D old-master
-# git branch -m master old-master
-# git branch -m deploy master
 git checkout master
 git merge -X theirs deploy
 
+# sync upstream changes
+git pull origin master
+
+# push local changes upstream
 git push origin master
 
+# go back to editing branch
 git checkout drafts
