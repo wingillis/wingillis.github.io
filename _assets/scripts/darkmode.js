eb87a43bc765167266c3a6aa399452a38d9a5a07
wingillis <win.gillis@gmail.com>
2019-02-12 00:32:21 -0500

    added dark mode to website, yay!!!

---
+++ _assets/scripts/darkmode.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('load', () => {
+  let mode = localStorage.getItem('mode');
+  document.getElementById('darkmode').checked = mode === 'dark';
+  mode === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
+})
+
+function updateDark() {
+  let item = document.getElementById('darkmode').checked;
+  localStorage.setItem('mode', item ? 'dark' : 'light' );
+  localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
+}
