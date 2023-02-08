9eba4da9af8a17912154431d1f1dcefcc03f4b93
wingillis <win.gillis@gmail.com>
2023-02-08 16:41:03 -0500

    better remaster script?

--- remaster.sh
+++ remaster.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+# make sure you are in the deploy branch
+
 set -e
 
-git checkout  master
-git merge -X theirs deploy
+# git checkout master
+git checkout deploy
+# git merge -X theirs deploy
 
 # sync upstream changes
-git pull origin master
+git merge -s ours master
+
+git checkout master
+
+git merge deploy
 
 # push local changes upstream
 git push origin master
